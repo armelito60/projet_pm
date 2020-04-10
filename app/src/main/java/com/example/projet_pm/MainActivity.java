8bea8f37135edbf6ffb1a57860636278ce87d2f2
armelito60 <tchiasso@et.esiea.fr>
2020-04-10 16:57:21 +0200

    création API football : implémentation MatchAPI

--- app/src/main/java/com/example/projet_pm/MainActivity.java
+++ app/src/main/java/com/example/projet_pm/MainActivity.java
@@ -5,12 +5,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String BASE_URL = "https://www.scorebat.com/";
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -20,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        showList();
+        makeApiCall();
+    }
+
+    private void showList() {
         // cherche l'id recycler_view et instanciation avec la variable recyclerView
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // taille fixe du recyclerView
@@ -38,6 +54,41 @@ public class MainActivity extends AppCompatActivity {
         // on donne la liste input à la liste mAdapter
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
+    }
 
+    private void makeApiCall() {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        MatchAPI matchAPI = retrofit.create(MatchAPI.class);
+
+        Call<Racine> call = matchAPI.getRacine();
+        call.enqueue(new Callback<Racine>() {
+            @Override
+            public void onResponse(Call<Racine> call, Response<Racine> response) {
+                if(response.isSuccessful() && response.body() != null) {
+                    List<Match> matchList = response.body().getFootball();
+                    Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Racine> call, Throwable t) {
+                showError();
+            }
+        });
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/projet_pm/MatchAPI.java
+++ app/src/main/java/com/example/projet_pm/MatchAPI.java
@@ -1,4 +1,10 @@
 package com.example.projet_pm;
 
-public class MatchAPI {
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MatchAPI {
+    @GET("/video-api/v1")
+    Call<Racine> getRacine();
 }
